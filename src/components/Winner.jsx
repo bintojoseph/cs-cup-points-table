9113d2eca7ceb8212f5872906901d22002e047d6
Riya Sabu <riyasabures@gmail.com>
2024-01-31 21:27:19 +0530

    added new slider

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -2,9 +2,9 @@ import Posts from "./Posts";
 
 const images = [
     // links
-    // "https://i.ibb.co/2d4cW4q/1.png",
-    // "https://i.ibb.co/2d4cW4q/2.png",
-    // "https://i.ibb.co/2d4cW4q/3.png",
+    "https://i.ibb.co/2d4cW4q/1.png",
+    "https://i.ibb.co/2d4cW4q/2.png",
+    "https://i.ibb.co/2d4cW4q/3.png",
   ];
 
 function Winner() {
